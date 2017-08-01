6121f7b883a143259b00f904e3fdd4fc72559808
Jay Merlan <jr.merlan@gmail.com>
2017-07-31 20:39:04 -0700

    Combined CREATE TABLE and INSERT graphs into single graph. Cleaned up and archived files.

--- Sandbox/Revit Data to SQLite/DB/sql_SelectDifferences.sql
+++
@@ -1,5 +0,0 @@
-SELECT p.ElementId, p.Height, p.Width, p.Diameter, p.Length, ap.ElementId, ap.Height, ap.Width, ap.Diameter, ap.Length
-FROM Ducts_1499005456 p
-JOIN Ducts_1499005601 ap
-ON p.ElementId = ap.ElementId
-WHERE p.Width != ap.Width OR p.Height != ap.Height
